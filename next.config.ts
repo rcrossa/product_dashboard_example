486d3b09a8e7f4f1f6f34ddae58214e422ff0f76
Roberto Rossa <rcrossa@hotmail.com>
2025-11-19 12:28:59 -0300

    update project configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['bcryptjs', '@prisma/client', '@prisma/adapter-pg', 'pg'],
+  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
